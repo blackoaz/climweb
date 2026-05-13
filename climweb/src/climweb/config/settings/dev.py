782f1f2f1102ac34257ad6befe7c0f013366f230
blackoaz <pauloakello56@gmail.com>
2026-05-13 21:21:59 +0300

    updating docker-compose file

--- climweb/src/climweb/config/settings/dev.py
+++ climweb/src/climweb/config/settings/dev.py
@@ -1,13 +1,14 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-5=&i=f&w$_2=ktbhw43anl(uxgue*-i23r!1uibrh9l7-$q-1#"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
